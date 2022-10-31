b25aac559d038cb3fa24258b7c764efec2536667
sahasourav17 <sahasourav1170@gmail.com>
2022-11-01 00:51:58 +0600

    add solution of Linked List Cycle problem

---
+++ LinkedListCycle.py
@@ -0,0 +1,15 @@
+# Floyd tortoise and hare method
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        '''
+		slow and fast pointer will start from same position at the beginning.
+		But slow pointer will move one position at each iteration while fast pointer moves two position.
+		So, eventually fast pointer (hare) will catch up the slow pointer (tortoise) if there any cycle exists.
+		'''
+        slowPointer,fastPointer = head,head
+        while fastPointer and fastPointer.next:
+            slowPointer = slowPointer.next
+            fastPointer = fastPointer.next.next
+            if fastPointer==slowPointer:
+                return True
+        return False
