9d5a1c0c354b835a7f26eb4ff51174066cd4fdc8
sahasourav17 <sahasourav1170@gmail.com>
2022-11-04 02:25:12 +0600

    add O(N) solution of Matrix Diagonal Sum.

--- matrixDiagonalSum.py
+++ matrixDiagonalSum.py
@@ -17,3 +17,28 @@ class Solution:
             j -= 1
 
         return sum
+
+'''
+    O(N) solution.
+    Best and Efficient solution.
+'''
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        sum = 0
+        n = len(mat)
+        j = n-1
+
+        for i in range(n):
+            '''
+                Adding the value of diagonals
+            '''
+            sum += mat[i][i] + mat[i][j-i]
+
+        '''
+            If the matrix is even square then it has no common diagonal element.
+            So, return the sum as it is.
+            Else, there is a common diagonal element. So, we need to 
+            subtract that element value from sum.
+        '''
+        return sum if n%2 == 0 else sum - mat[int(j/2)][int(j/2)]
