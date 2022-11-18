8caf23afcc5308eb7ec2921b380ba16335d960a4
sahasourav17 <sahasourav1170@gmail.com>
2022-11-18 23:22:15 +0600

    add easy and concise solution of Ugly Numbers.

--- UglyNumbers.py
+++ UglyNumbers.py
@@ -19,3 +19,20 @@ class Solution:
             if check == False:
                 return False
         return True
+
+
+'''
+    Easy and Concise Solution
+'''
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        uglyFactors = [2,3,5]
+        for i in uglyFactors:
+            while n % i == 0:
+                n /= i
+
+        return True if n == 1 else False
