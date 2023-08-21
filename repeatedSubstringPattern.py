7aee1508b70cc5f8dfec0a97aacc834e1341e296
sahasourav17 <sahasourav1170@gmail.com>
2023-08-21 18:34:31 +0600

    add O(n) solution of 459. Repeated Substring Pattern

--- repeatedSubstringPattern.py
+++ repeatedSubstringPattern.py
@@ -1,11 +1,30 @@
+# Approach 1
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        subStr = ""
         sLength = len(s)
-        for i in range(0, sLength-1):
-            subStr += s[i]
-            subStrLength = len(subStr)
-            if sLength % subStrLength == 0 and subStr*(sLength // subStrLength) == s:
+        for i in range(1, sLength//2+1):
+            subStr = s[:i]
+            if sLength % i == 0 and subStr*(sLength // i) == s:
                 return True
 
         return False
+
+# Time Complexity : O(N^2)
+# Space Complexity : o(N)
+
+
+# Approach 2
+
+"""
+    step 1: concatenate the same string twice
+    step 2: remove first and last character
+    step 3: check if original string is present in the string obtained from step 2
+"""
+
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in (s+s)[1:-1]
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
