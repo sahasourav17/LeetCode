27095e8bf73fc607d7b47dd829cc600fe15aea22
Sourav Saha <42141718+sahasourav17@users.noreply.github.com>
2024-12-26 21:27:25 +0600

    add dp solution of 0494-target-sum.py

--- 0494-target-sum.py
+++ 0494-target-sum.py
@@ -18,3 +18,20 @@ class Solution:
             return dp[(i,cur_sum)]
         
         return backtrack(0,0)
+
+
+# Dynamic Programming
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
+
+        # (0 elements, 0 Sum) -> 1 way
+        # 1 way to sum to 0 with first 0 elements
+        dp[0][0] = 1
+
+        for i in range(len(nums)):
+            for cur_sum, count in dp[i].items():
+                dp[i + 1][cur_sum + nums[i]] += count
+                dp[i + 1][cur_sum - nums[i]] += count
+            # print(f"{i} -> {dp[i].items()}")
+        return dp[len(nums)][target]
