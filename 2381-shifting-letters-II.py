3d3316b4f8080610d99fede6de737dc8137abf22
Sourav Saha <42141718+sahasourav17@users.noreply.github.com>
2025-01-05 19:05:25 +0600

    add refactored solution of  2381-shifting-letters-II.py

--- 2381-shifting-letters-II.py
+++ 2381-shifting-letters-II.py
@@ -12,3 +12,26 @@ class Solution:
 
         return "".join(s)
 
+
+## Efficient Solution ( Prefix Sum)
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        n = len(s)
+        prefix_diff = [0] * (n + 1)
+
+        for left, right, d in shifts:
+            prefix_diff[right+1] += 1 if d else -1
+            prefix_diff[left] += -1 if d else 1
+
+        diff = 0
+        res = [ord(c) - ord("a") for c in s]
+
+        for i in reversed(range(n + 1)):
+            diff += prefix_diff[i]
+            res[i - 1] = (diff + res[i - 1]) % 26
+
+        s = [chr(ord("a") + n) for n in res]
+
+        return "".join(s)
+
+
